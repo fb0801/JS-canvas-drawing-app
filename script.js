fa49f25fd11c2b19e0107118d9b092025df42ad5
Farhan <74499472+fb0801@users.noreply.github.com>
2021-09-19 18:04:33 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
-const canvas = document.getElementById('canvas');
+const canvas = document.getElementById("canvas");
 canvas.width = window.innerWidth - 60;
 canvas.height = 400;
+
+let context = canvas.getContext("2d");
+context.fillstyle = "white";
+context.fillrect(0,0, canvas.width, canvas.height);
